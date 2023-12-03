e26a392ace0cd669bf2162d79d2f9329dd231682
wolterbwambale <benplaan1@gmail.com>
2023-12-04 00:37:01 +0300

    add navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,53 @@
-import React from 'react'
-import './Navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Footer from "../Footer/Footer";
 
 const Navbar = () => {
-  return (
-    <div>this is navbar</div>
-  )
-}
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-export default Navbar
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+      <div className="vertical-line">
+      <div className="nav-container">
+        <span className="left">Logo</span>
+        <div className={`navbar ${isMobileMenuOpen ? "active" : ""}`}>
+          <ul className={isMobileMenuOpen ? "active" : ""}>
+            <li>
+              <Link to="/">
+                <button className="btn btn-success">Home</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/feature">
+                <button className="btn btn-success">Feature</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Addform">
+                <button className="btn btn-success">AddItem</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Reservation">
+                <button className="btn btn-role btn-success">Reservation</button>
+              </Link>
+            </li>
+
+          </ul>
+          <div className="hamburger" onClick={toggleMobileMenu}>
+            <div className={`line ${isMobileMenuOpen ? "open" : ""}`}></div>
+            <div className={`line ${isMobileMenuOpen ? "open" : ""}`}></div>
+            <div className={`line ${isMobileMenuOpen ? "open" : ""}`}></div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Navbar;
