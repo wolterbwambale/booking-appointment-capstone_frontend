704cfbb12051a3cc9959d702a802f1d26ece5c9f
agneta1991 <pupienytea@gmail.com>
2023-12-06 15:20:59 +0200

    added styling, nav bar burger menu when screen smaller

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,47 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import Footer from '../Footer/Footer';
 
-const Navbar = () => (
-  <div className="vertical-line">
-    <div className="nav-container">
-      <span className="left">Logo</span>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">
-              <button type="button" className="btn btn-success">
-                Home
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/feature">
-              <button type="button" className="btn btn-success">
-                Feature
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Addform">
-              <button type="button" className="btn btn-success">
-                AddItem
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Reservation">
-              <button type="button" className="btn btn-role btn-success">
-                Reservation
-              </button>
-            </Link>
-          </li>
-        </ul>
+const Navbar = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const renderNavigation = () => {
+    if (windowWidth <= 375) {
+      return (
+        <>
+          <div className="nav-container">
+            <span className="left">Logo</span>
+            <img
+              className="burger"
+              src="https://img.icons8.com/ios/50/000000/menu--v1.png"
+              alt="menu--v1"
+            />
+          </div>
+        </>
+      );
+    }
+    return (
+      <div className="vertical-line">
+        <div className="nav-container">
+          <span className="left">Logo</span>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">
+                  <button type="button" className="btn btn-success">
+                    Home
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link to="/feature">
+                  <button type="button" className="btn btn-success">
+                    Feature
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link to="/Addform">
+                  <button type="button" className="btn btn-success">
+                    AddItem
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link to="/Reservation">
+                  <button type="button" className="btn btn-role btn-success">
+                    Reservation
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <Footer />
       </div>
-    </div>
-    <Footer />
-  </div>
-);
+    );
+  };
+
+  return renderNavigation();
+};
 
 export default Navbar;
