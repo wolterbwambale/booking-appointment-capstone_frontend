811fb917e8f862e493799a010fcd36b530ec8c7f
Bhekisile <bhekisilejozi@gmail.com>
2023-12-06 06:20:53 +0200

    Create doctor's Slice and fetch data from API

---
+++ src/store/doctors/doctorSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchDoctors = createAsyncThunk('doctors/fetch', async () => {
+  const doctors = await fetch.POST('http://127.0.0.1:4000/api/v1/doctors');
+  const data = await doctors.json();
+  return data;
+});
+
+const initialState = {
+  doctors: '',
+  error: '',
+  loading: false,
+};
+
+const doctorSlice = createSlice({
+  name: 'doctors',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchDoctors.pending, (state) => {
+        const newState = { ...state, loading: true };
+        return newState;
+      })
+      .addCase(fetchDoctors.fulfilled, (state, action) => {
+        const newState = { ...state, doctors: action.payload, loading: false };
+        return newState;
+      })
+      .addCase(fetchDoctors.rejected, (state) => {
+        const newState = {
+          ...state,
+          error: 'Error 404. Failed to fetch',
+          loading: false,
+        };
+        return newState;
+      });
+  },
+});
+
+export default doctorSlice.reducer;
