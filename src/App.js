8a95cd584db366eba105d2c1b9446e6ed119da06
wolterbwambale <benplaan1@gmail.com>
2023-12-06 20:10:12 +0300

    add store in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import store from './store';
 import {
   Feature,
   Footer,
@@ -11,18 +13,21 @@ import {
 } from './components/index';
 
 function App() {
+  const doctorId = 2;
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/feature" element={<Feature />} />
-        <Route path="/addform" element={<Addform />} />
-        <Route path="/reservation" element={<Reservation />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/feature" element={<Feature doctorId={doctorId} />} />
+          <Route path="/addform" element={<Addform />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Provider>
   );
 }
 
