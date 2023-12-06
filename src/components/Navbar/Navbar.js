4c86a0d63defd4c0ddee3aba593d988c838b0de6
agneta1991 <pupienytea@gmail.com>
2023-12-06 20:49:33 +0200

    Added pop up menu for mobile version when burger menu is clicked

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,64 +18,97 @@ const Navbar = () => {
     };
   }, []);
 
-  const renderNavigation = () => {
-    if (windowWidth <= 375) {
-      return (
-        <>
+  const LinksComponent = () => {
+    const [showLinks, setShowLinks] = useState(false);
+
+    const toggleLinks = () => {
+      setShowLinks(!showLinks);
+    };
+
+    const renderNavigation = () => {
+      if (windowWidth <= 375) {
+        return (
           <div className="nav-container">
             <span className="left">Logo</span>
-            <img
+            <button
+              type="button"
+              onClick={toggleLinks}
               className="burger"
-              src="https://img.icons8.com/ios/50/000000/menu--v1.png"
-              alt="menu--v1"
-            />
+              aria-label="Toggle Menu"
+            >
+              <img
+                src="https://img.icons8.com/ios/50/000000/menu--v1.png"
+                alt="menu--v1"
+              />
+            </button>
+            {showLinks && (
+              <div className="pop-up">
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/feature">Feature</Link>
+                  </li>
+                  <li>
+                    <Link to="/Addform">AddItem</Link>
+                  </li>
+                  <li>
+                    <Link to="/Reservation">Reservation</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
-        </>
-      );
-    }
-    return (
-      <div className="vertical-line">
-        <div className="nav-container">
-          <span className="left">Logo</span>
-          <div>
-            <ul>
-              <li>
-                <Link to="/">
-                  <button type="button" className="btn btn-success">
-                    Home
-                  </button>
-                </Link>
-              </li>
-              <li>
-                <Link to="/feature">
-                  <button type="button" className="btn btn-success">
-                    Feature
-                  </button>
-                </Link>
-              </li>
-              <li>
-                <Link to="/Addform">
-                  <button type="button" className="btn btn-success">
-                    AddItem
-                  </button>
-                </Link>
-              </li>
-              <li>
-                <Link to="/Reservation">
-                  <button type="button" className="btn btn-role btn-success">
-                    Reservation
-                  </button>
-                </Link>
-              </li>
-            </ul>
+        );
+      }
+
+      return (
+        <div className="vertical-line">
+          <div className="nav-container">
+            <span className="left">Logo</span>
+            <div>
+              <ul>
+                <li>
+                  <Link to="/">
+                    <button type="button" className="btn btn-success">
+                      Home
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/feature">
+                    <button type="button" className="btn btn-success">
+                      Feature
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/Addform">
+                    <button type="button" className="btn btn-success">
+                      AddItem
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/Reservation">
+                    <button type="button" className="btn btn-role btn-success">
+                      Reservation
+                    </button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    );
+      );
+    };
+
+    return renderNavigation();
   };
 
-  return renderNavigation();
+  return <LinksComponent />;
 };
 
 export default Navbar;
