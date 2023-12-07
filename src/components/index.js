add6c308d7dd716d0c90e10f4d796dca92e79f5a
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-12-07 14:59:21 +0300

    linter more fix

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Home from './home/Home';
 import Navbar from './Navbar/Navbar';
 import Login from './Login/Login';
 import Addform from '../containers/addform/Addform';
-import Reservation from '../containers/Reservationform/Reservation';
+import Reservation from '../containers/reservationform/reservation';
 
 export {
   Feature, Footer, Home, Navbar, Login, Addform, Reservation,
