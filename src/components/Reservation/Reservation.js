d9ca2aadc2563c309361df01dff5fb82176db8c2
ybadr99 <yousefbadr909@gmail.com>
2023-12-07 10:20:06 +0200

    remove unsed files

--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import './reservation.css';
 
-const Reservation = () => (
-  <div>reservation</div>
-);
+const Reservation = () => <div>reservation</div>;
 
 export default Reservation;
