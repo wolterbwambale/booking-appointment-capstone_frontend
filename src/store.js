d1be097105fae0e10d683cf68720b00401b8fc7d
Bhekisile <bhekisilejozi@gmail.com>
2023-12-12 14:38:13 +0200

    Import add doctor reducer

--- src/store.js
+++ src/store.js
@@ -11,15 +11,3 @@ const store = configureStore({
   },
 });
 export default store;
-
-// store
-// import { configureStore } from '@reduxjs/toolkit';
-// import doctorReducer from './doctors/doctorSlice';
-
-// const store = configureStore({
-//   reducer: {
-//     doctors: doctorReducer,
-//   },
-// });
-
-// export default store;
