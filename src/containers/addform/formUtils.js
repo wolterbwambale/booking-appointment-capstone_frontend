bf319bd8b6f9f2791260f799d4c4ca420bb62128
Bhekisile <bhekisilejozi@gmail.com>
2023-12-12 14:34:31 +0200

    Appending form values

--- src/containers/addform/formUtils.js
+++ src/containers/addform/formUtils.js
@@ -5,4 +5,19 @@ const createDoctorFormData = (doctorData, userId) => {
   formData.append('doctor[years_of_experience]', doctorData.years_of_experience);
   formData.append('doctor[price_per_appointment]', doctorData.price_per_appointment);
 
- 
+  if (doctorData.images) {
+    for (let i = 0; i < doctorData.images.length; i += 1) {
+      formData.append('doctor[images][]', doctorData.images[i]);
+    }
+  }
+
+  if (userId) {
+    formData.append('doctor[user_id]', userId);
+  } else {
+    throw new Error('User is not logged in.');
+  }
+
+  return formData;
+};
+
+export default createDoctorFormData;
